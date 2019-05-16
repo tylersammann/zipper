ad76a0b54108727d2e07f637eb78bfe02910237c
tylersammann <tylersammann@gmail.com>
2019-05-16 02:03:39 -0500

    added lorca and file dialog

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
+//go:generate go run -tags generate generate.go
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/tylersammann/zipper/static"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/zserge/lorca"
+)
+
+func setFile(fileId, jsonBytes string) {
+	fmt.Printf("file id %s\n", fileId)
+	fmt.Printf("file data %s\n", jsonBytes)
+}
 
 func main() {
-	fmt.Println("hey hey")
+	// Start up the browser window
+	ui, err := lorca.New("", "", 480, 320)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
 
+	ui.Bind("setFile", setFile)
 
+	// Serve the static files
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go http.Serve(ln, http.FileServer(static.FS))
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
+
+	// Wait until UI window is closed
+	<-ui.Done()
 }
